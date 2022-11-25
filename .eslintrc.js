56610f19c32d647a236e2fad52e51fb26bca576e
Marco Bouwkamp <marco.bouwkamp@mendix.com>
2022-11-25 10:58:38 +0100

    update .eslintrc.js and create release package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 const base = require("@mendix/pluggable-widgets-tools/configs/eslint.ts.base.json");
 
 module.exports = {
-    ...base
+    ...base,
+    "rules": {
+        // Note: you must disable the base rule as it can report incorrect errors
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "warn"
+      }
 };
